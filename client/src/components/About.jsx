61eba245cf5976d097f9e9b03196705d4d5ccf8c
Code451F <451fsolo@gmail.com>
2023-04-12 22:29:34 +0530

    constants page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -41,7 +41,7 @@ const ProjectCard = ({
 
           <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
             <div
-              onClick={() => window.open(source_code_link, "_blank")}
+              onClick={() => window.open(source_code_link, "_self")}
               className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer border-2 border-[#ff8f00]'
             >
               {/* <img
--- client/src/pages/Evolvhack.jsx
+++ client/src/pages/Evolvhack.jsx
@@ -21,13 +21,13 @@ const Evolvhack = () => {
       <div className={`xl:mt-0 flex xl:flex-col flex-col items-center overflow-hidden bg-primary relative w-full h-screen relative z-0`}>
         <motion.div
           variants={slideIn("left", "tween", 0.2, 1)}
-          // initial={{ x: "-100%" }}
-          // animate={{ x: 0 }}
-          // transition={{ duration: 0.5 }}
-          className={'flex flex-col bg-transparent p-8 rounded-2xl border-4 border-[#ff8f00] text-center mb-20 mt-10 w-3/4'}
+          initial={{ x: "-100%" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className='flex-col text-center mb-20 mt-10 w-3/4 bg-transparent p-8 rounded-2xl border-4 border-[#ff8f00]'
         >
           <p className={styles.sectionSubText}>Guidelines and Regulation</p>
-          <h3 className={styles.sectionHeadText}>What is <span className='text-[#ffffff]'>Evolv</span>Hackathon 2023?</h3>
+          <h3 className={styles.sectionHeadText}>What is <span className='text-[#ffffff]'>Evolv</span>Hack 2023?</h3>
         </motion.div>
         <StarsCanvas/>
       </div>
@@ -50,3 +50,4 @@ className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-b
 </div> */}
 
 // xl:bg-transparent
+// className={'flex flex-col bg-transparent p-8 rounded-2xl border-4 border-[#ff8f00] text-center mb-20 mt-10 w-3/4'}
